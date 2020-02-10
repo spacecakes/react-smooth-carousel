672855e7405bd3a1308b10d0856b54c0949ffcb9
Gabriel Lundmark <g.lundmark@gmail.com>
2020-02-10 14:19:52 +0100

    Initial commit

---
+++ src/components/Carousel/CarouselView.js
@@ -0,0 +1,39 @@
+/** @jsx jsx */
+import { bool, node, number } from "prop-types";
+import { jsx } from "theme-ui";
+
+const CarouselView = ({ children, vertical, slidesVisible, slideGap }) => {
+  return (
+    <div
+      sx={{
+        gridArea: "1 / 1 / -1 / -1",
+        gridGap: slideGap,
+        display: "grid",
+        position: "relative",
+        gridAutoFlow: vertical ? "row" : "column",
+        gridAutoColumns: vertical ? "100%" : `${100 / slidesVisible}%`,
+        gridAutoRows: vertical ? `${100 / slidesVisible}%` : "100%",
+        size: "100%",
+        overflowY: vertical ? "auto" : "hidden",
+        overflowX: vertical ? "hidden" : "auto",
+        scrollSnapType: `${vertical ? "y" : "x"} mandatory`,
+        scrollbarWidth: "none", // Hide scrollbar on Firefox
+        "-webkit-overflow-scrolling": "touch", // Needed to work on iOS Safari
+        "::-webkit-scrollbar": { display: "none" } // Hide scrollbar on Chrome & Safari
+      }}
+      /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+      tabIndex="0"
+    >
+      {children}
+    </div>
+  );
+};
+
+CarouselView.propTypes = {
+  children: node.isRequired,
+  slideGap: number.isRequired,
+  slidesVisible: number.isRequired,
+  vertical: bool.isRequired
+};
+
+export default CarouselView;
